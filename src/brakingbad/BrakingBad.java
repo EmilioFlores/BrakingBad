06eaedbf3e6ad16028f73843717cf672cb084b21
Emilio Flores <emilio_wassa@hotmail.com>
2014-03-03 11:04:56 -0600

    Proyecto
    braking bad

---
+++ src/brakingbad/BrakingBad.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package brakingbad;
+
+/**
+ *
+ * @author Emilio
+ */
+public class BrakingBad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
